09e0821292411abd6516a3a2007c10fa1c25799f
Amin Babar <amin.babar1@gmail.com>
2022-07-25 15:59:22 -0400

    hello:

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 
 
+// compare.then(ele => console.log(ele))
 document.addEventListener("DOMContentLoaded", ()=>{
+
+
+    window.Andy = Andy;
     // const ele = document.getElementById("main");
     const height = 600;
     const width = 900;
+    const translate = [width / 2, height / 1.4];
+    const scale = 140;
 
-    const projection = d3.geoMercator();
+
+    const projection = d3.geoMercator().scale(scale).translate(translate);
     const path = d3.geoPath(projection);
 
     const svg = d3.select('body')
@@ -18,6 +25,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
         .then(data => {
             const countries = topojson.feature(data, data.objects.countries);
+            // debugger;
             g.selectAll("path"
                 ).data(countries.features)
                 .enter()
@@ -25,6 +33,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 .attr('class', 'country')
                 .attr('d', path);
 
+            d3
+                .selectAll('.country')
+                ._groups[0]
+                .forEach(ele => {
+                    ele.addEventListener("click", function () {
+                        debugger;
+                        ele.classList.add('blue');
+                        alert(ele.__data__.properties.name);
+                    })
+                })
+            // debugger;
+            
+
         });
+
+    
 
 });
