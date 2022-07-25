0f06eb69830e2e06d902934cd07bd3405b1afe1a
Amin Babar <amin.babar1@gmail.com>
2022-07-25 11:50:43 -0400

    map rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    const ele = document.getElementById("main");
-    debugger;
+    // const ele = document.getElementById("main");
+    const height = 600;
+    const width = 900;
+
+    const projection = d3.geoMercator();
+    const path = d3.geoPath(projection);
+
+    const svg = d3.select('body')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+
+    // grouping of all the countries with path common?
+    const g = svg.append('g');
+    d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+        .then(data => {
+            const countries = topojson.feature(data, data.objects.countries);
+            g.selectAll("path"
+                ).data(countries.features)
+                .enter()
+                .append('path')
+                .attr('class', 'country')
+                .attr('d', path);
+
+        });
+
 });
